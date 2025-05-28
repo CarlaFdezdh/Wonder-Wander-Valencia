1cbf295b2bac35eedb7296007a6db7948f87b974
Marcos Pardo <105744591+MarParTor@users.noreply.github.com>
2025-05-28 09:52:42 +0200

    Traducción de la web a inglés

--- www/verRutas.php
+++ www/verRutas.php
@@ -10,13 +10,15 @@ if ($_POST["minkm"]== ''){
 }else{
   $miquery3 = $_POST["minkm"] ?? 0;
 }
-// Conexión a la base de datos
 $bd = "rutas.db";
 $conn = new SQLite3($bd, SQLITE3_OPEN_READONLY);
 
 // Usamos consulta preparada para evitar inyecciones
 
-  $stmt = $conn->prepare("SELECT * FROM ruta WHERE nombre LIKE :nombre AND distancia < :distmax and distancia > :distmin");
+  $stmt = $conn->prepare("SELECT * FROM ruta 
+                        WHERE nombre LIKE :nombre 
+                        AND distancia <= :distmax 
+                        AND distancia >= :distmin");
   $stmt->bindValue(':nombre', '%' . $miquery . '%', SQLITE3_TEXT);
   $stmt->bindValue(':distmax', $miquery2, SQLITE3_INTEGER);
   $stmt->bindValue(':distmin', $miquery3, SQLITE3_INTEGER);
@@ -41,14 +43,14 @@ $result = $stmt->execute();
     <nav>
         <ul>
             <li><a href="index.html">HOME</a></li>
-            <li><a href="#">BUSCA TU RUTA</a></li>
-            <li><a href="addRuta.html">INGRESA TU RUTA</a></li>
-            <li><a href="conocenos.html">CONOCENOS</a></li>
+            <li><a href="#">FIND YOUR TRAIL</a></li>
+            <li><a href="addRuta.html">UPLOAD YOUR TRAIL</a></li>
+            <li><a href="conocenos.html">CONTACT US</a></li>
         </ul>
     </nav>
   <form action="verRutas.php" method="post" id="queryform">
     <div id="distanciasf">
-    <input type="text" id="queryN" name="queryN" placeholder="Buscar rutas..." value="<?= htmlspecialchars($miquery) ?>">
+    <input type="text" id="queryN" name="queryN" placeholder="Search trails..." value="<?= htmlspecialchars($miquery) ?>">
     <div>
     <!--<label for="minkm" >Min (Km)</label>-->
     <input type="number" id="minkm" name="minkm" placeholder="Min km" min="0">
@@ -58,7 +60,7 @@ $result = $stmt->execute();
     <input type="number" id="maxkm" name="maxkm" placeholder="Max km" min="0">
     </div>
     </div>
-    <input type="submit" value="Buscar">
+    <input type="submit" value="Search">
   </form>
   <main id="vr">
   <?php while ($row = $result->fetchArray(SQLITE3_ASSOC)): ?>
@@ -66,31 +68,30 @@ $result = $stmt->execute();
       <h2><?= htmlspecialchars($row['nombre']) ?></h2>
       <div class="card-body">
         <div>
-        <p><strong>Distancia:</strong> <span class="attrib"> <?= htmlspecialchars($row['distancia']) ?> km</span></p>
-        <p><strong>Recomendado:</strong></p>
+        <p><strong>Distance:</strong> <span class="attrib"> <?= htmlspecialchars($row['distancia']) ?> km</span></p>
+        <p><strong>Recommended:</strong></p>
           <?php if ($row['caminando']==1):
-            echo "<span class='attrib'>Caminando</span>"
+            echo "<span class='attrib'>Walking</span>"
             ?>
           <?php endif; ?>
           <?php if ($row['bici']==1): 
-            echo "<span class='attrib'>En bici</span>"
+            echo "<span class='attrib'>By bike</span>"
             ?>
           <?php endif; ?>
           <?php if ($row['mascota']==1):
-            echo "<span class='attrib'>Con mascota</span>"
+            echo "<span class='attrib'>With pet</span>"
             ?>
           <?php endif; ?>
           <?php if ($row['silla']==1):
-            echo "<br><span class='attrib'>En silla de ruedas</span>"
+            echo "<br><span class='attrib'>In a wheelchair</span>"
             ?>
           <?php endif; ?>
           </div>
             <?php foreach ($row as $key => $item): ?>
             <?php if ($key === 'imagen'): ?>
-            <img src="imgSubidas/<?= htmlspecialchars($item) ?>" alt="Imagen de la ruta" height="220" width="330">
+            <img src="imgSubidas/<?= htmlspecialchars($item) ?>" alt="Pic of the trail" height="220" width="330">
             <?php endif; ?>
             <?php if ($key === 'mapa'): ?>
-            <!-- esto es un placeholder-->
             <?php echo "<iframe frameBorder='0' scrolling='no' src='".$row['mapa']."' width='500' height='400'></iframe>" ?>
             <?php endif; ?>
           <?php endforeach; ?>
